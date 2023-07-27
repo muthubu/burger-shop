cdfd70009cebb585d7a1b1e0dcc2b2dbaa62b0d4
Muthaiahprojects <muthubu01@gmail.com>
2023-07-27 15:20:35 +0530

    commit itemslist

--- bshop/urls.py
+++ bshop/urls.py
@@ -23,6 +23,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home),
+    path("items/",views.items,name="items"),
     path('t/',views.trys),
 ]
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
