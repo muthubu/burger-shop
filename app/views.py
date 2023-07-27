8a520e7968cfecdece54d383541d38af55302c36
Muthaiahprojects <muthubu01@gmail.com>
2023-07-27 15:34:05 +0530

    commit error

--- app/views.py
+++ app/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render
 def home(request):
    return render(request,'home.html')
 
+def items(request):
+   return render(request,"itemslist.html")   
+
 def trys(request):
    return render(request,'try.html')
 
